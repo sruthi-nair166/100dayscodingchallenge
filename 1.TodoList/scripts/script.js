38f68825ed6471830e832149b59cb16fb8767ff1
Sruthi <sruthivnair78@gmail.com>
2025-12-18 22:16:56 +0530

    add edit tasks functionality

--- 1.TodoList/scripts/script.js
+++ 1.TodoList/scripts/script.js
@@ -3,21 +3,58 @@ const addBtn = document.getElementById("add");
 const tasksBox = document.querySelector("#tasks");
 const tasksEmpty = document.querySelector(".tasks-empty");
 
+let isEditing = false;
+let editTaskBox = null;
 let taskArray = [];
 
 addBtn.addEventListener("click", () => {
-  if (input.value.trim()) {
-    taskArray.push(input.value.trim());
+  if (!isEditing) {
+    if (input.value.trim()) {
+      taskArray.push(input.value.trim());
 
-    tasksEmpty.classList.toggle("hidden", taskArray.length !== 0);
+      tasksEmpty.classList.toggle("hidden", taskArray.length !== 0);
 
-    tasksBox.innerHTML += `<div class="task-box">
-      <p>${input.value}</p>
-      <button>Edit</button>
+      tasksBox.innerHTML += `<div class="task-box">
+      <p class="input-value">${input.value}</p>
+      <button class="edit">Edit</button>
     </div>`;
+    } else {
+      alert("Please enter a task before trying to add.");
+      return;
+    }
+    input.value = "";
   } else {
-    alert("Please enter a task before trying to add.");
-    return;
+    if (input.value.trim()) {
+      for (let i = 0; i < taskArray.length; i++) {
+        if (
+          taskArray[i] === editTaskBox.querySelector(".input-value").textContent
+        ) {
+          taskArray[i] = input.value;
+          editTaskBox.querySelector(".input-value").textContent = input.value;
+          console.log(taskArray);
+        }
+      }
+    } else {
+      alert("Please enter a task before trying to add.");
+      return;
+    }
+    input.value = "";
+    addBtn.textContent = "Add Task";
+    isEditing = false;
   }
-  input.value = "";
 });
+
+tasksBox.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("edit")) return;
+
+  handleEdit(e);
+});
+
+function handleEdit(e) {
+  input.focus();
+  isEditing = true;
+  editTaskBox = e.target.parentElement;
+  input.value = editTaskBox.querySelector(".input-value").textContent;
+  addBtn.textContent = "Save";
+  console.log(editTaskBox);
+}
