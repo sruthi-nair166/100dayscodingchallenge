a2a35ff7d3a6509e762ca20153bc755601472f6b
Sruthi <sruthivnair78@gmail.com>
2025-12-26 16:37:17 +0530

    add accessibility

--- 2.SidebarMenu/scripts.js/script.js
+++ 2.SidebarMenu/scripts.js/script.js
@@ -2,21 +2,33 @@ const menuBtn = document.getElementById("menu-btn");
 const navMenu = document.querySelector("nav");
 const closeBtn = document.getElementById("close-btn");
 const overlay = document.getElementById("overlay");
+const navLinks = document.querySelectorAll("nav a");
 
-/*const screenWidth = window.matchMedia("(min-width: 769px)");
+const screenWidth = window.matchMedia("(width < 768px)");
 
-screenWidth.addEventListener("change", () => {
-  navMenu.classList.remove("show");
-});*/
+screenWidth.addEventListener("change", (e) => updateNavbar(e));
+
+function updateNavbar(e) {
+  const isMobile = e.matches;
+  if (isMobile) {
+    navMenu.setAttribute("inert", "");
+  } else {
+    navMenu.removeAttribute("inert", "");
+  }
+}
 
 function openMenu() {
   navMenu.classList.add("show");
   overlay.classList.add("show");
+  menuBtn.setAttribute("aria-expanded", true);
+  navMenu.removeAttribute("inert", "");
 }
 
 function closeMenu() {
   navMenu.classList.remove("show");
   overlay.classList.remove("show");
+  menuBtn.setAttribute("aria-expanded", false);
+  navMenu.setAttribute("inert", "");
 }
 
 menuBtn.addEventListener("click", openMenu);
@@ -24,3 +36,9 @@ menuBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
 
 overlay.addEventListener("click", closeMenu);
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+updateNavbar(screenWidth);
